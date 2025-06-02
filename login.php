3bf73d1bea51c56b71e8906d33308af26bc3f25d
UnsoaringSky <kimxaviercabrera@gmail.com>
2025-06-02 19:25:04 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$con = mysqli_connect("localhost","root","","user");
+$usertrim = trim($_POST['username']);
+$userstrip = stripcslashes($usertrim);
+$finaluser = htmlspecialchars($userstrip);
+
+$passtrim = trim($_POST['password']);
+$passstrip = stripcslashes($passtrim);
+$finalpass = htmlspecialchars($passstrip);
+
+$sql = "SELECT * FROM user_tbl where username = '$finaluser' AND password = '$finalpass'";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result)>=1)
+{
+ $_SESSION["myuser"]= $finaluser;
+ header("Location:dashboard.html");
+}
+else{
+    $_SESSION["error"]= "You are not valid user";
+    header("Location:error.html");
+}
+?>
